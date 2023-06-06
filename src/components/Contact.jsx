89591609955c31442de0308ad85ad8642452c251
Thanh Qui <nguyenhtqui@gmail.com>
2023-06-06 14:37:15 +0200

    fix: scroll on click expand arrow

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -174,7 +174,7 @@ const Contact = () => {
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
-            {loading ? 'Sending...' : 'Send'}
+            {loading ? 'Envoi en cours ...' : 'Envoyer'}
           </button>
         </form>
       </motion.div>
