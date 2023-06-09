47092329e3bc6e4c8fecee4115be722fc638462f
Thanh Qui <nguyenhtqui@gmail.com>
2023-06-09 10:56:19 +0200

    fix: try fix img 3D hero by device

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,9 +6,7 @@ import CanvasLoader from "../Loader";
 import { useMediaQuery } from "react-responsive";
 
 const Computers = ({ isMobile, isTabletVertical }) => {
-  // const computer = useGLTF("./desktop_pc/scene.gltf");
   const computer = useGLTF("./coffeemat/scene.gltf");
-
 
   return (
     <mesh>
@@ -45,28 +43,42 @@ const ComputersCanvas = () => {
   const isMobile = useMediaQuery({ query: '(max-width: 640px)' });
   const isTabletVertical = useMediaQuery({ query: '(max-width: 820px)' });
 
-  return (
-    <Canvas
-      frameloop='demand'
-      shadows
-      dpr={[1, 2]}
-      // camera={{ position: [20, 3, 5], fov: 25 }}
-      camera={{ position: [850, 200, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          autoRotate={true}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 3}
-          minPolarAngle={Math.PI / 3}
-        />
-        <Computers isMobile={isMobile} isTabletVertical={isTabletVertical} />
-      </Suspense>
+  const isChrome = window?.navigator?.userAgentData.brands.find(b => b.brand.includes('Chrome'));
 
-      <Preload all />
-    </Canvas>
-  );
+  if (window?.navigator?.userAgentData.platform === 'Android') {
+    // const computer = useGLTF("./desktop_pc/scene.gltf");
+
+    // console.log("agent is:", window?.navigator?.userAgent)
+
+    // console.log("platform is: ", window?.navigator?.platform)
+
+    // console.log(window?.navigator?.userAgentData)
+    return <></>;
+  } else {
+
+    return (
+      <Canvas
+        frameloop='demand'
+        shadows
+        dpr={[1, 2]}
+        // camera={{ position: [20, 3, 5], fov: 25 }}
+        camera={{ position: [850, 200, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            autoRotate={true}
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 3}
+            minPolarAngle={Math.PI / 3}
+          />
+          <Computers isMobile={isMobile} isTabletVertical={isTabletVertical} />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    );
+  }
 };
 
 export default ComputersCanvas;
